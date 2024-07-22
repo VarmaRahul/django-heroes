aab67ca454f9774dbf4c1c46489b699a7cc7e0ac
Rahul Varma <rv111421@gmail.com>
2024-07-22 17:22:30 +0530

    added project

---
+++ myproject/avengers/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
+
+# Create your views here.
+def heros(request):
+    hero = Avengers.objects.all().order_by('id')
+
+    return render(request, 'avengers/heros.html', {'hero': hero})
